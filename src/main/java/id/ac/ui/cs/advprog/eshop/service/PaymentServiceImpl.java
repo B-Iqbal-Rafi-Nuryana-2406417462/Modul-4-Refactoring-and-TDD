f578ec0283b2ac90fe5dfbd962a51ad528a7e859
Iqbal <raflyiqbal1409@gmail.com>
2026-03-10 13:59:33 +0700

    [REFACTOR] Refactoring PaymentService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,41 +1,47 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
-public class PaymentServiceImpl implements PaymentService{
+public class PaymentServiceImpl implements PaymentService {
 
-    @Autowired
-    private PaymentRepository paymentRepository;
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(final PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 
     @Override
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        String id = UUID.randomUUID().toString();
-        Payment payment = new Payment(id, method, paymentData, order);
+    public Payment addPayment(final Order order, final String method, final Map<String, String> paymentData) {
+        final String id = UUID.randomUUID().toString();
+        final Payment payment = new Payment(id, method, paymentData, order);
         return paymentRepository.save(payment);
     }
 
     @Override
-    public Payment setStatus(Payment payment, String status) {
+    public Payment setStatus(final Payment payment, final String status) {
         payment.setStatus(status);
 
-        if ("SUCCESS".equals(status)) {
-            payment.getOrder().setStatus("SUCCESS");
-        } else if ("REJECTED".equals(status)) {
-            payment.getOrder().setStatus("FAILED");
+        if (PaymentStatus.SUCCESS.getValue().equals(status)) {
+            payment.getOrder().setStatus(PaymentStatus.SUCCESS.getValue());
+        } else if (PaymentStatus.REJECTED.getValue().equals(status)) {
+            payment.getOrder().setStatus(OrderStatus.FAILED.getValue());
         }
 
         return paymentRepository.save(payment);
     }
 
     @Override
-    public Payment getPayment(String paymentId) {
+    public Payment getPayment(final String paymentId) {
         return paymentRepository.findById(paymentId);
     }
 
